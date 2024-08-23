a5373a7cb58f361e58ebc4208980c913a32e260b
Ardy1an <adithardiyanto1@students.amikom.ac.id>
2024-08-24 00:22:12 +0700

    memperbaiki rute ke halaman starting pages

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:coffee_shop/theme.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -13,21 +14,41 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     Timer(Duration(seconds: 2),
-        (() => Navigator.pushReplacementNamed(context, '/homepage')));
+        (() => Navigator.pushReplacementNamed(context, '/starting')));
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body : Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/bg_kopi.png'),
-            fit: BoxFit.cover
-            )
+
+        backgroundColor: buttonColor2,
+        body: Center(
+          child: RichText(text: TextSpan(
+            children: <TextSpan>[
+              TextSpan(
+                text: 'Coffee', style: title1
+              ),
+              TextSpan(
+                text: 'Shop',
+                style: TextStyle(
+                  color: buttonColor2,
+                  fontWeight: semiBold,
+                  fontSize: 50,
+                )
+              )
+            ]
+          )),
         ),
-      )
+
+      // body : Container(
+      //   decoration: BoxDecoration(
+      //     image: DecorationImage(
+      //       image: AssetImage('assets/bg_kopi.png'),
+      //       fit: BoxFit.cover
+      //       )
+      //   ),
+      // )
     );
   }
 

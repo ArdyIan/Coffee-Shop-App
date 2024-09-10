e4eef9659278734d5f3b6d50ccfa79c4eb6b711c
Ardy1an <adithardiyanto0@gmail.com>
2024-09-11 00:27:48 +0700

    menambahkan named route baru

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:coffee_shop/pages/detail_product.dart';
+import 'package:coffee_shop/pages/home_page.dart';
+import 'package:coffee_shop/pages/order.dart';
 import 'package:coffee_shop/pages/splash_page.dart';
 import 'package:coffee_shop/pages/starting_page.dart';
 import 'package:flutter/material.dart';
@@ -10,15 +13,16 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    routes: {
-      '/': (context) => SplashPage(),
-      '/starting' : (context) => StartingPage(),
-    },
-  );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/starting': (context) => StartingPage(),
+        '/homepage' : (context) => HomePage(),
+        '/detailproduct' : (context) => detailProduct(),
+        '/order' : (context) => Order(),
+      },
+    );
+  }
 }
-}
-
-
